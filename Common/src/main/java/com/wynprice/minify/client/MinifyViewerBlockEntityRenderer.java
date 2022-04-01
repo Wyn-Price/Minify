597e223449bd91b6e227bc1e0ee324ce453e5cb5
Wyn Price <wynprice999@gmail.com>
2022-04-01 05:29:46 +0100

    Better way of not using AO when rendering

--- Common/src/main/java/com/wynprice/minify/client/MinifyViewerBlockEntityRenderer.java
+++ Common/src/main/java/com/wynprice/minify/client/MinifyViewerBlockEntityRenderer.java
@@ -3,6 +3,9 @@ package com.wynprice.minify.client;
 import com.mojang.blaze3d.vertex.*;
 import com.mojang.math.Vector3f;
 import com.wynprice.minify.blocks.entity.MinifyViewerBlockEntity;
+import net.minecraft.CrashReport;
+import net.minecraft.CrashReportCategory;
+import net.minecraft.ReportedException;
 import net.minecraft.client.AmbientOcclusionStatus;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.ItemBlockRenderTypes;
@@ -13,10 +16,13 @@ import net.minecraft.client.renderer.block.ModelBlockRenderer;
 import net.minecraft.client.renderer.blockentity.BlockEntityRenderDispatcher;
 import net.minecraft.client.renderer.blockentity.BlockEntityRenderer;
 import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
+import net.minecraft.client.renderer.texture.OverlayTexture;
+import net.minecraft.client.resources.model.BakedModel;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.block.RenderShape;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.material.FluidState;
+import net.minecraft.world.phys.Vec3;
 
 import java.util.Random;
 
@@ -47,9 +53,6 @@ public class MinifyViewerBlockEntityRenderer implements BlockEntityRenderer<Mini
 
         stack.pushPose();
 
-        AmbientOcclusionStatus ambientOcclusion = Minecraft.getInstance().options.ambientOcclusion;
-        Minecraft.getInstance().options.ambientOcclusion = AmbientOcclusionStatus.OFF;
-
         stack.translate(0.5, 0.5, 0.5);
         stack.mulPose(Vector3f.YP.rotationDegrees(90 * blockEntity.getHorizontalRotationIndex()));
         stack.translate(-0.5, -0.5, -0.5);
@@ -66,7 +69,6 @@ public class MinifyViewerBlockEntityRenderer implements BlockEntityRenderer<Mini
             });
         });
 
-        Minecraft.getInstance().options.ambientOcclusion = ambientOcclusion;
 
         stack.popPose();
     }
@@ -101,7 +103,22 @@ public class MinifyViewerBlockEntityRenderer implements BlockEntityRenderer<Mini
 
                     stack.pushPose();
                     stack.translate(pos.getX() & 15, pos.getY() & 15, pos.getZ() & 15);
-                    blockRenderDispatcher.renderBatched(state, pos, level, stack, buffer, true, random);
+
+                    //Essentially, call blockRenderDispatcher.renderBatched but without ao
+//                    blockRenderDispatcher.renderBatched(state, pos, level, stack, buffer, true, random);
+                    try {
+                        RenderShape shape = state.getRenderShape();
+                        if(shape == RenderShape.MODEL) {
+                            Vec3 offset = state.getOffset(level, pos);
+                            stack.translate(offset.x, offset.y, offset.z);
+                            this.blockRenderDispatcher.getModelRenderer().tesselateWithoutAO(level, this.blockRenderDispatcher.getBlockModel(state), state, pos, stack, buffer, true, random, state.getSeed(pos), OverlayTexture.NO_OVERLAY);
+                        }
+                    } catch (Throwable var11) {
+                        CrashReport $$9 = CrashReport.forThrowable(var11, "Tesselating block model");
+                        CrashReportCategory $$10 = $$9.addCategory("Block being tesselated");
+                        CrashReportCategory.populateBlockDetails($$10, level, pos, state);
+                        throw new ReportedException($$9);
+                    }
                     stack.popPose();
                 }
             }
